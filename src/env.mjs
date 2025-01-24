fcf2e6b0e7a9c98e4fec6e7de7cc10f38b9f0343
Driek van der Meulen <driekvandermeulen@icloud.com>
2025-01-24 16:58:57 +0100

    fix?

--- src/env.mjs
+++ src/env.mjs
@@ -1,21 +1,11 @@
-import { createEnv } from "@t3-oss/env-nextjs";
-import { z } from "zod";
+import { z } from 'zod';
 
-export const env = createEnv({
-  server: {
-    CONSTRUCTION: z.preprocess((str) => str === "true" || str === "false" ? str === "true" : str, z.boolean()),
-    IS_VIDEO_PUBLIC: z.preprocess((str) => str === "true" || str === "false" ? str === "true" : str, z.boolean()),
-    IS_FIGMA_PUBLIC: z.preprocess((str) => str === "true" || str === "false" ? str === "true" : str, z.boolean()),
-  },
-  client: {
-    FIGMA_LINK: z.string().url(),
-    VIDEO_LINK: z.string().url(),
-  },
-  runtimeEnv: {
-    CONSTRUCTION: process.env.CONSTRUCTION,
-    IS_VIDEO_PUBLIC: process.env.IS_VIDEO_PUBLIC,
-    IS_FIGMA_PUBLIC: process.env.IS_FIGMA_PUBLIC,
-    FIGMA_LINK: process.env.FIGMA_LINK,
-    VIDEO_LINK: process.env.VIDEO_LINK,
-  },
+const envSchema = z.object({
+  FIGMA_LINK: z.string().url(),
+  VIDEO_LINK: z.string().url(),
+  CONSTRUCTION: z.boolean().optional(),
+  IS_VIDEO_PUBLIC: z.boolean().optional(),
+  IS_FIGMA_PUBLIC: z.boolean().optional(),
 });
+
+export const env = envSchema.parse(process.env);
