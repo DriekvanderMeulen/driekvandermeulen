908b91944d9fc61bd9dc16fe38f352a7edda3405
Driek van der Meulen <driekvandermeulen@icloud.com>
2025-01-24 17:15:08 +0100

    link cnahege

--- src/app/stagevideo/page.tsx
+++ src/app/stagevideo/page.tsx
@@ -4,7 +4,7 @@ import Auth from '@/components/auth/page-auth';
 
 const Page = () => {
     useEffect(() => {
-        window.location.href = "https://www.figma.com/deck/CNqpAKpyzslbS6jF6ZvPnn/Stageverslag-Driek?node-id=12002-315&t=scg8ESfeur90QUUv-1"
+        window.location.href = "https://www.figma.com/deck/CNqpAKpyzslbS6jF6ZvPnn/Stageverslag-Driek?node-id=12132-239&t=scg8ESfeur90QUUv-1"
     }, []);
 
     return null;
