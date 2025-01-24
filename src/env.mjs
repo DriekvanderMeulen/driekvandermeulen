d13e9ea9bd5c58fc294300a1d491f6b5976dbdf5
Driek van der Meulen <driekvandermeulen@icloud.com>
2025-01-24 17:01:50 +0100

    commit

--- src/env.mjs
+++ src/env.mjs
@@ -3,9 +3,9 @@ import { z } from 'zod';
 const envSchema = z.object({
   FIGMA_LINK: z.string().url(),
   VIDEO_LINK: z.string().url(),
-  CONSTRUCTION: z.boolean().optional(),
-  IS_VIDEO_PUBLIC: z.boolean().optional(),
-  IS_FIGMA_PUBLIC: z.boolean().optional(),
+  CONSTRUCTION: z.preprocess((val) => val === 'true', z.boolean().optional()),
+  IS_VIDEO_PUBLIC: z.preprocess((val) => val === 'true', z.boolean().optional()),
+  IS_FIGMA_PUBLIC: z.preprocess((val) => val === 'true', z.boolean().optional()),
 });
 
 export const env = envSchema.parse(process.env);
