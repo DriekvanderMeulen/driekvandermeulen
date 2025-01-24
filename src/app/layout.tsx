9c8d4fea797f7ec1fe016b3e99cac0ae7985cfef
Driek van der Meulen <driekvandermeulen@icloud.com>
2025-01-24 16:51:50 +0100

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import DarkModeToggle from "@/components/functions/dark-mode";
-import PianoClient from '@/components/functions/piano-client';
 
 export const metadata: Metadata = {
   title: "Create Next App",
--- src/app/stageverslag/page.tsx
+++ src/app/stageverslag/page.tsx
@@ -1,16 +1,20 @@
+"use client"
+import { useEffect } from 'react';
 import Auth from '@/components/auth/page-auth';
 import { env } from '@/env.mjs'
 
-export default function Page() {
+const Page = () => {
+    useEffect(() => {
+        window.location.href = env.FIGMA_LINK;
+    }, []);
+
+    return null;
+};
+
+export default function AuthenticatedPage() {
     return (
         <Auth>
-            <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden', maxWidth: '100%' }}>
-                <iframe
-                    src={env.FIGMA_LINK}
-                    style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', border: 'none' }}
-                    allowFullScreen
-                ></iframe>
-            </div>
+            <Page />
         </Auth>
     );
 }
