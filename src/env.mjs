d9cad0d104b36935182ff9496c8baca959618f0c
Driek van der Meulen <driekvandermeulen@icloud.com>
2025-01-24 17:06:34 +0100

    fix?

--- src/env.mjs
+++ src/env.mjs
@@ -3,9 +3,6 @@ import { z } from 'zod';
 const envSchema = z.object({
   FIGMA_LINK: z.string().url(),
   VIDEO_LINK: z.string().url(),
-  CONSTRUCTION: z.preprocess((val) => val === 'true', z.boolean().optional()),
-  IS_VIDEO_PUBLIC: z.preprocess((val) => val === 'true', z.boolean().optional()),
-  IS_FIGMA_PUBLIC: z.preprocess((val) => val === 'true', z.boolean().optional()),
 });
 
 export const env = envSchema.parse(process.env);
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { env } from '@/env.mjs'
 
 export function middleware(request: NextRequest) {
-  // Get the pathname
-  const { pathname } = request.nextUrl
-
-  // Construction mode check
-  if (env.CONSTRUCTION === true) {
-    if (pathname !== '/construction') {
-      return NextResponse.redirect(new URL('/construction', request.url))
-    }
-  }
-
-  // Stage video access check
-  if (env.IS_VIDEO_PUBLIC === false && pathname === '/stagevideo') {
-    return NextResponse.redirect(new URL('/', request.url))
-  }
-
-  // Stage figma access check
-  if (env.IS_FIGMA_PUBLIC === false && pathname === '/stageverslag') {
-    return NextResponse.redirect(new URL('/', request.url))
-  }
 
     // Check for dark mode preference
     const prefersDarkMode = request.headers.get('sec-ch-prefers-color-scheme') === 'dark'
