1dad0e951028a67e0556e414f16b7109d7fa0cb4
Driek van der Meulen <driekvandermeulen@icloud.com>
2025-02-01 00:28:13 +0100

    lmc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,12 @@
 export default function Page() {
 
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="fixed inset-0 flex items-center justify-center">
+        <div className=" p-4 rounded">
+          Let me cook...
+        </div>
+      </div>
     </div>
   );
 }
