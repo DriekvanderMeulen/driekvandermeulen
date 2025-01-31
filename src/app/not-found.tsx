ca6be0f6ede1a92cce4632a45e5a7cac2e078405
Driek van der Meulen <driekvandermeulen@icloud.com>
2025-01-31 14:21:23 +0100

    fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,32 +1,9 @@
-"use client"
+import dynamic from 'next/dynamic'
 
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/navigation'
+const NotFoundClient = dynamic(() => import('@/components/NotFoundClient'), {
+    ssr: false
+})
 
 export default function NotFound() {
-    const [countdown, setCountdown] = useState(3)
-    const router = useRouter()
-
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            router.push('/')
-        }, 3000)
-
-        const interval = setInterval(() => {
-            setCountdown((prev) => prev - 1)
-        }, 1000)
-
-        return () => {
-            clearTimeout(timer)
-            clearInterval(interval)
-        }
-    }, [router])
-
-    return (
-        <div className="flex h-screen items-center justify-center">
-            <span className="font-inter text-2xl">
-                Not found, redirecting in {countdown}
-            </span>
-        </div>
-    )
+    return <NotFoundClient />
 }
